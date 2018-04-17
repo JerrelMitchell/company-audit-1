921dab560848ac16ef377cfba2b6df33af6387d1
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-17 12:00:40 -0600

    add tests for company file

--- test/company_test.rb
+++ test/company_test.rb
@@ -5,6 +5,7 @@ require './lib/company'
 class CompanyTest < Minitest::Test
   def setup
     @employee_file = './data/employees.csv'
+    @bad_employee_file = './data/bad_employees.csv'
     @company = Company.new
   end
 
@@ -18,8 +19,33 @@ class CompanyTest < Minitest::Test
     assert_equal [], @company.timesheets
   end
 
-  def test_it_loads_data
+  def test_it_does_not_load_data_with_failure_hash
+    result = @company.load_employees(@bad_employee_file)
+    expected = { success: false, error: 'bad data' }
+    assert_equal expected, result
+    assert_equal [], @company.employees
+  end
+
+  def test_it_loads_data_with_success_hash
     result = @company.load_employees(@employee_file)
-    assert_equal Hash, result
+    expected = { success: true, error: nil }
+    assert_equal expected, result
+    assert_equal 'Susan Smith', @company.employees.first.name
+  end
+
+  def test_it_can_find_an_employee_with_given_id
+    #failing test
+    skip
+    @company.load_employees(@employee_file)
+    result = @company.find_employee_by_id(2)
+    assert_equal 'John Smith', result.name
+  end
+
+  def test_it_can_find_a_project_with_a_given_id
+    #failing test
+    skip
+    @company.load_projects(@employee_file)
+    result = @company.find_project_by_id(1)
+    assert_equal 'Widgets', result.name
   end
 end
