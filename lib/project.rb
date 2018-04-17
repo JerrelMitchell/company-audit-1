dd10f6c9718f6f3776a9dd27164503a918c6b03b
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-17 09:33:25 -0600

    add attribute readers and initialize methods. tests passing

---
+++ lib/project.rb
@@ -0,0 +1,9 @@
+class Project
+  attr_reader :id, :name, :start_date, :end_date
+  def initialize(id, name, start_date, end_date)
+    @id = id.to_i
+    @name = name
+    @start_date = start_date
+    @end_date = end_date
+  end
+end
