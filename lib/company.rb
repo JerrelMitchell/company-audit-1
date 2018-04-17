c5405b383a5625586de4538b602774cb8d27a30c
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-17 12:00:08 -0600

    add updated methods for iteration 2

--- lib/company.rb
+++ lib/company.rb
@@ -20,4 +20,17 @@ class Company
     load_attributes(filename, @timesheets, Timesheet)
   end
 
+  def find_employee_by_id(id)
+    find_by_id(@employees, id)
+  end
+
+  def find_project_by_id(id)
+    find_by_id(@projects, id)
+  end
+
+  def find_by_id(attributes, id)
+    attributes.select do |attribute|
+      attribute.id == id
+    end.first
+  end
 end
--- lib/timesheet.rb
+++ lib/timesheet.rb
@@ -6,4 +6,16 @@ class Timesheet
     @date = date
     @minutes = minutes.to_i
   end
+
+  def self.validate(data)
+    return false if data.length != 5
+    return false if data[0].to_i.to_s != data[0]
+    begin
+      DateHandler.string_to_date data[3]
+      DateHandler.string_to_date data[4]
+    rescue ArgumentError
+      return false
+    end
+    true
+  end
 end
